1b826fdc685b33c66b02cc040b6de12684275603
zackzhangCN <2512704509@qq.com>
2021-04-23 16:00:37 +0800

    使用nacos配置中心进行配置管理

--- UserInfoProvider1/src/main/java/cn/zack/controller/UserInfoProviderController.java
+++ UserInfoProvider1/src/main/java/cn/zack/controller/UserInfoProviderController.java
@@ -1,6 +1,7 @@
 package cn.zack.controller;
 
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
  * 支持dubbo调用和rest调用
  */
 @RestController
-@Service(protocol = { "dubbo", "rest" })
+@Service(protocol = {"dubbo", "rest"})
 public class UserInfoProviderController {
+
+    @Value("${config.test}")
+    private String configTest;
 
     @GetMapping(path = "fun1")
     public String fun1() {
-        return "调用了userInfoProvider1, 方法: fun1";
+        return "调用了userInfoProvider1, 方法: fun1, configTest=" + configTest;
     }
 }
